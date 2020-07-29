ea0b70c5e052f1bc89851fb00325c0ed7d19966e
arseniy_pomazkov <arpomazkov@bk.ru>
2020-07-29 21:48:20 +0300

    Added header, footer templates and date module

--- app.js
+++ app.js
@@ -1,40 +1,44 @@
-const express = require(`express`);
-const bodyParser = require(`body-parser`);
+const express = require('express');
+const bodyParser = require('body-parser');
+const date = require(__dirname + '/date.js')
 
 const app = express();
 
-let listOfTasks = ['Buy food', 'Cook food', 'Eat food'];
+const listOfTasks = ['Buy food', 'Cook food', 'Eat food'];
+const listOfWorkingTasks = [];
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static('public'))
 
 app.get('/', function(req, res) {
-  let today = new Date();
-
-  let options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long'
-  };
-
-  let day = today.toLocaleDateString('en-US', options);
-
   res.render('list', {
-    kindOfDay: day,
+    listTitle: date.getDate(),
     newListItems: listOfTasks
   })
 })
 
-app.post('/', function(req, res) {
-  let newTask = req.body.newItem;
-  listOfTasks.push(newTask);
-  res.redirect('/');
-  // res.render('list', {
-  //   listOfTasks: currentTasks
-  // })
+app.get('/work', function(req, res) {
+  res.render('list', {
+    listTitle: 'Work list',
+    newListItems: listOfWorkingTasks
+  })
 })
+
+app.post('/', function(req, res) {
+  const newTask = req.body.newItem;
+  if (req.body.list === 'Work') {
+    listOfWorkingTasks.push(newTask);
+    res.redirect('/work');
+  } else {
+    listOfTasks.push(newTask);
+    res.redirect('/');
+  }
+})
+
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
