7a4af13e7cd31e1dcf2527335d7f6d0022c5ba23
arseniy_pomazkov <arpomazkov@bk.ru>
2020-08-05 14:49:30 +0300

    Updated .gitignore, package.json  and port log message in app.js

--- app.js
+++ app.js
@@ -140,5 +140,5 @@ app.post('/delete', function(req, res) {
 
 
 app.listen(process.env.PORT || 3000, function() {
-  console.log("Server started on port 3000");
+  console.log("Server started successfully");
 });
